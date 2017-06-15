c91b9a73e3ee2261faefd95de744557087e96dcc
Maximilian Wagenbach <maximilian.wagenbach@native-instruments.de>
2017-06-15 20:47:46 +0200

    Aufgabe C

---
+++ Hausaufgabe_1/Matlab/C80.m
@@ -0,0 +1,10 @@
+function [c80] = C80 (signal, fs)
+    s_80 = 80 * fs / 1000;
+    
+    signal = signal.^2;
+    
+    num = sum(signal(1:s_80));
+    denom = sum(signal(s_80:end));
+
+    c80 = 10 * log10(num/denom);
+end
--- Hausaufgabe_1/Matlab/main.m
+++ Hausaufgabe_1/Matlab/main.m
@@ -13,22 +13,22 @@ T30 = find(EDC_norm < -30,1)/fs;
 T20 = find(EDC_norm < -20,1)/fs;
 
 %find first peak before maximum
-[max1, maxIndex] = max(abs(y));
-peakIndex = findchangepts(y(1:maxIndex-1), 'Statistic', 'linear');
+[max1, maxIndex] = max(abs(x));
+peakIndex = findchangepts(x(1:maxIndex-1), 'Statistic', 'linear');
 
 T60_1 = 2*(T30-peakIndex/fs);
 T60_2 = 3*(T20-peakIndex/fs);
 
 
 figure;
-plot(abs(y));
+plot(abs(x));
 
 %% Aufgabe B)
 
-x_125  = oktavBand(y, 125, fs);
-x_250  = oktavBand(y, 250, fs);
-x_500  = oktavBand(y, 500, fs);
-x_1000 = oktavBand(y, 100, fs);
+x_125  = oktavBand(x, 125, fs);
+x_250  = oktavBand(x, 250, fs);
+x_500  = oktavBand(x, 500, fs);
+x_1000 = oktavBand(x, 1000, fs);
 
 T_125  = 3*((find(EDC(x_125) < -20,1)/fs)-peakIndex/fs);
 T_250  = 3*((find(EDC(x_250) < -20,1)/fs)-peakIndex/fs);
@@ -41,4 +41,7 @@ BR = (T_125 + T_250) / (T_500 + T_1000);
 
 %% Aufgabe C)
 
-s_80 = 44100 / 1000 * 80;
+C80_125  = C80(x_125, fs);
+C80_250  = C80(x_250, fs);
+C80_500  = C80(x_500, fs);
+C80_1000 = C80(x_1000, fs);
