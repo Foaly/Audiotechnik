54f1787036962ce0d76b01631547242f4b5511f1
Maximilian Wagenbach <maximilian.wagenbach@native-instruments.de>
2017-06-15 15:54:45 +0200

    Added comments.

--- Hausaufgabe_1/Matlab/main.m
+++ Hausaufgabe_1/Matlab/main.m
@@ -1,22 +1,27 @@
-
+%% Aufgabe A
 
 [y,Fs] = audioread('Hausaufgabe_1_Impulsantwort.wav');
 
+% square signal
 yq = y.^2;
-sum1 = sum(yq);
-R = sum1 - cumsum(yq);
 
+% discretize Schröder integral
+R = sum(yq) - cumsum(yq);
+
+% normalized energy decay curve
 EDC_norm = 10 * log10(R/sum1);
 
 figure;
 plot(EDC_norm);
 
+
+% find T20 & T30 time point
 T30 = find(EDC_norm < -30,1)/Fs;
 T20 = find(EDC_norm < -20,1)/Fs;
 
 %find first peak before maximum
 [max1, maxIndex] = max(abs(y));
-peakIndex = findchangepts(y(1:maxIndex-1), 'Statistic', 'linear')
+peakIndex = findchangepts(y(1:maxIndex-1), 'Statistic', 'linear');
 
 T60_1 = 2*(T30-peakIndex/Fs);
 T60_2 = 3*(T20-peakIndex/Fs);
