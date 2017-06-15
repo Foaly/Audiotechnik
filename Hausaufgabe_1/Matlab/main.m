3755384393e4978dbedca1e5204ed2027d4c571a
Anyere Bendrien <anyere.bendrien@posteo.de>
2017-06-15 20:24:34 +0200

    Refactoring

--- Hausaufgabe_1/Matlab/main.m
+++ Hausaufgabe_1/Matlab/main.m
@@ -1,38 +1,44 @@
-%% Aufgabe A
+%% Aufgabe A)
 
-[x,Fs] = audioread('Hausaufgabe_1_Impulsantwort.wav');
+[x,fs] = audioread('Hausaufgabe_1_Impulsantwort.wav');
 
 % plot energy decay curve
 figure;
-plot(EDC(x));
+EDC_norm = EDC(x);
+plot(EDC_norm);
 
 
 % find T20 & T30 time point
-T30 = find(EDC_norm < -30,1)/Fs;
-T20 = find(EDC_norm < -20,1)/Fs;
+T30 = find(EDC_norm < -30,1)/fs;
+T20 = find(EDC_norm < -20,1)/fs;
 
 %find first peak before maximum
 [max1, maxIndex] = max(abs(y));
 peakIndex = findchangepts(y(1:maxIndex-1), 'Statistic', 'linear');
 
-T60_1 = 2*(T30-peakIndex/Fs);
-T60_2 = 3*(T20-peakIndex/Fs);
+T60_1 = 2*(T30-peakIndex/fs);
+T60_2 = 3*(T20-peakIndex/fs);
 
 
 figure;
 plot(abs(y));
 
-%% B
+%% Aufgabe B)
 
-x_125  = oktavBand(y, 125, Fs);
-x_250  = oktavBand(y, 250, Fs);
-x_500  = oktavBand(y, 500, Fs);
-x_1000 = oktavBand(y, 100, Fs);
+x_125  = oktavBand(y, 125, fs);
+x_250  = oktavBand(y, 250, fs);
+x_500  = oktavBand(y, 500, fs);
+x_1000 = oktavBand(y, 100, fs);
 
-T_125  = 3*((find(EDC(x_125) < -20,1)/Fs)-peakIndex/Fs);
-T_250  = 3*((find(EDC(x_250) < -20,1)/Fs)-peakIndex/Fs);
-T_500  = 3*((find(EDC(x_500) < -20,1)/Fs)-peakIndex/Fs);
-T_1000 = 3*((find(EDC(x_1000) < -20,1)/Fs)-peakIndex/Fs);
+T_125  = 3*((find(EDC(x_125) < -20,1)/fs)-peakIndex/fs);
+T_250  = 3*((find(EDC(x_250) < -20,1)/fs)-peakIndex/fs);
+T_500  = 3*((find(EDC(x_500) < -20,1)/fs)-peakIndex/fs);
+T_1000 = 3*((find(EDC(x_1000) < -20,1)/fs)-peakIndex/fs);
 
 
 BR = (T_125 + T_250) / (T_500 + T_1000);
+
+
+%% Aufgabe C)
+
+s_80 = 44100 / 1000 * 80;
