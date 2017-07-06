15f09ad618a9da0757a4f188c8aa888658261e47
Anyere Bendrien <anyere.bendrien@posteo.de>
2017-07-06 18:49:25 +0200

    Init HA2

---
+++ Hausaufgabe_2/Matlab/main.m
@@ -0,0 +1,13 @@
+%%  Technische Akustik - Zweite Hausaufgabe
+%% 
+%%  Anyere Bendrien 382004
+%%  anyere.bendrien@campus.tu-berlin.de
+%%  
+%%  Maximilian Wagenbach 382110
+%%  m.wagenbach@campus.tu-berlin.de
+%%  
+%%  Jason Horn 382104
+%%  j.horn@campus.tu-berlin.de
+
+%% Aufgabe 1
+%% a)
