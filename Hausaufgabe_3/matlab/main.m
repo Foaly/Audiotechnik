b8a8fdd1e1bb36716a52583ba081a40259df385b
Maximilian Wagenbach <maximilian.wagenbach@native-instruments.de>
2017-08-14 17:14:47 +0200

    Added plots for C

--- Hausaufgabe_3/matlab/main.m
+++ Hausaufgabe_3/matlab/main.m
@@ -102,10 +102,25 @@ G_abs = abs(G(freqs,wc,Qtc));
 % convert to dB values
 G_dB = 20*log10(G_abs);
 % plot G(f)
+
 figure;
 semilogx(freqs/(2*pi),G_dB);
-hold on;
+axis([0 10000 -90 10]);
+%title('Normaler Frequenzgang');
 grid on;
+ylabel('Amplitude in dB');
+xlabel('Frequenz in Hz');
+hold on;
+% mark resonance frequency
+fc = wc / (2*pi);
+fcY = interp1(freqs/(2*pi), G_dB, fc);
+stem(fc, fcY, '--ro', 'BaseValue', -90);
+% mark -3 db cutoff
+y = interp1(G_dB, freqs/(2*pi), -3);
+stem(y, -3, '--r*');
+%save plot
+print('Normaler_Frequenzgang', '-depsc');
+
 % calculate volume of box
 Vb = 480/((wc/ws)^2-1);
 % find Vb3 iteratively 
@@ -134,11 +149,41 @@ while max1 <= 1
 end
 Vb1
 
+figure;
 semilogx(freqs/(2*pi),G_dB3);
+axis([0 10000 -90 10])
+%title('Frequenzgang bei 3 dB Überhöhung')
+grid on;
+ylabel('Amplitude in dB');
+xlabel('Frequenz in Hz');
+hold on;
+% mark resonance frequency
+fc3 = wc3 / (2*pi);
+fc3Y = interp1(freqs/(2*pi), G_dB3, fc3);
+stem(fc3, fc3Y, '--ro', 'BaseValue', -90);
+% mark -3 db cutoff
+y = interp1(G_dB3, freqs/(2*pi), -3);
+stem(y, -3, '--r*');
+%save plot
+print('Frequenzgang_3dB', '-depsc');
+
+figure;
 semilogx(freqs/(2*pi),G_dB1);
-hold off;
-
-
+axis([0 10000 -90 10])
+%title('Frequenzgang bei 1 dB Überhöhung');
+grid on;
+ylabel('Amplitude in dB')
+xlabel('Frequenz in Hz')
+hold on;
+% mark resonance frequency
+fc1 = wc1 / (2*pi);
+fc1Y = interp1(freqs/(2*pi), G_dB1, fc1);
+stem(fc1, fc1Y, '--ro', 'BaseValue', -90);
+% mark -3 db cutoff
+y = interp1(G_dB1, freqs/(2*pi), -3);
+stem(y, -3, '--r*');
+%save plot
+print('Frequenzgang_1dB', '-depsc');
 
 
 
